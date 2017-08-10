640e8ae50e55b63ff6fef3d96f99ea1fd47888f4
TJ Ayodele <tjayodele@gmail.com>
2017-08-09 20:03:52 -0500

    built out frame from mailchimp functionality

--- libs/mailchimp.js
+++ libs/mailchimp.js
@@ -1,3 +1,13 @@
-/**
- * Created by TJ on 8/7/2017.
- */
+var fs = require('fs');
+var keyfile = require('~/keys.json');
+var keys = JSON.parse(fs.readFileSync(keyfile,'utf8'));
+var Mailchimp = require('mailchimp');
+var mailchimp = new Mailchimp(keys.mailchimp_key);
+
+self = {
+
+	sendEmailtoList: function(sList,sTemplate,aContent,callback) {
+		callback();
+	},
+
+};
