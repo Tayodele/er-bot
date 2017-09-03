d20c5414fa2e6546744b60d6d16edeb7b60e9e3d
TJ Ayodele <tjayodele@gmail.com>
2017-09-02 21:15:59 -0500

    using promise

--- libs/mailchimp.js
+++ libs/mailchimp.js
@@ -8,20 +8,24 @@ var coreList = "eaa08a2c7b";
 self = {
 
 	sendEmailtoList: function(aContent,callback) {
-		mailchimp.get("/lists/"+coreList+"/members", function(err, result){
+		mailchimp.get("/lists/"+coreList+"/members")
+		.then(function(result){
 			var members = JSON.parse(result).members;
 			console.log(members);
 			members.forEach(function(mem){
 				if(typeof(mem) != "undefined"){
-					mailchimp.post("/automations/f4491285cd/emails/41895f2486/queue",mem.email_address,function(err,res){
-						if(err){
-							console.log(err);
-						}
-					});
+					mailchimp.post("/automations/f4491285cd/emails/41895f2486/queue",mem.email_address)
+					.then()
+					.catch(function(err){
+						console.log(err);
+					})
 				}
 			});
 			callback();
 		});
+		.catch(function(err){
+			console.log(err);
+		})
 	}
 };
 
